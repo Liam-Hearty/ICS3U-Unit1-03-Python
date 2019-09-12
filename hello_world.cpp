123f03d3195354ee4f768fd9be0094f4ebaa934c
Ubuntu <ubuntu@ip-172-31-17-81.ec2.internal>
2019-09-12 20:37:29 +0000

    added program

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+// Copyright(c) 2019 Liam Hearty All rights reserved
+//
+// Created by: Liam Hearty
+// Created on: September 2019
+// This is the "Hello, World!" program, but with nice style.
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl; 
+}
